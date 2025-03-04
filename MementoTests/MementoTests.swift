117d9aef2460ce8de04902d4f63ebede5ee26336
romanus <romanusdev@gmail.com>
2025-03-04 18:41:09 +0900

    Initial Commit

---
+++ MementoTests/MementoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MementoTests.swift
+//  MementoTests
+//
+//  Created by romanus on 3/4/25.
+//
+
+import Testing
+@testable import Memento
+
+struct MementoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
